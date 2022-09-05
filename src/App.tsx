4ad1e464e199798f267c7a1739f6a7d759251438
Radi Rusadi <radi.rusadi.s@gmail>
2022-09-05 23:22:40 +0700

    Created basic redux toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Counter from "./features/counter/counter.component";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Counter />
     </div>
   );
 }
